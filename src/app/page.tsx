bb69c49ce14a1c24c710bb10071c7cc8b472c395
thomasop <tdss33@hotmail.com>
2023-07-09 14:52:58 +0200

    Edit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,7 @@
 import { useEffect } from "react";
 
 export default function Home() {
-  useEffect(() => {
-    const fetcht = async () => {
-      let reponse = await fetch('/api/user')
-      let json = await reponse.json()
-      console.log(json)
-    }
-    fetcht()
-  }, [])
+
   return (
     <main>
       <section>
